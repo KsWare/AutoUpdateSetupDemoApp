6742f528f7a7c92c0a496974441f2dc67cc28d53
SchreinerK <SchreinerK@users.noreply.github.com>
2024-08-01 11:58:14 +0200

    .

--- build/Update-Version.psm1
+++ build/Update-Version.psm1
@@ -1,55 +1,44 @@
-function Update-Version {
-	[CmdletBinding()]
-	
-	$isPR = $env:isPR
-	Write-Output "START: Update-Version()"
-	Write-Output "isPR: $isPR"
-	
-	# Init AppVeyor API request 
-	$apiUrl = 'https://ci.appveyor.com/api'
-	$appveyorApiRequestHeaders = @{
+
+# Init AppVeyor API request 
+function Init-AppVeyorApiRequest {
+	$env:AppveyorApiUrl = 'https://ci.appveyor.com/api'
+	$env:AppveyorApiRequestHeaders = @{
 		"Authorization" = "Bearer $env:AppVeyorApiToken"
 		"Content-type" = "application/json"
 		"Accept" = "application/json"
-	}	
+	}
+}
 
-	# Read Settings
+function Read-AppveyorSettings {
+    # Read Settings
     if($isPR -eq $false) {
         $response = Invoke-RestMethod -Method Get -Uri "$apiUrl/projects/$env:APPVEYOR_ACCOUNT_NAME/$env:APPVEYOR_PROJECT_SLUG/settings" -Headers $appveyorApiRequestHeaders
-        $settings = $response.settings        
+        $env:AppveyorSettings = $response.settings        
     } else {
         # dummy settings
-        $settings = @{versionFormat = $env:APPVEYOR_BUILD_VERSION}        
+        $env:AppveyorSettings = @{versionFormat = $env:APPVEYOR_BUILD_VERSION}        
     }
-	
-    # Extract version format
-    $currentVersionSegments = $env:APPVEYOR_BUILD_VERSION.Split(".")
-    $major = $currentVersionSegments[0]
-    $minor = $currentVersionSegments[1]
-    $patch = $currentVersionSegments[2]
-    $buildVersion = "$major.$minor.$patch"
-    $buildNumber = $env:APPVEYOR_BUILD_NUMBER  
-    if($isPR -eq $false) {
-    	$versionFormat = $settings.versionFormat	
-    	Write-Output "versionFormat: $versionFormat"	
-    	if(-not ($versionFormat -match "^(\d+\.\d+\.\d+)\..+$")) {
-    	    Write-Error -Message "`nERROR: Unsupported version format!" -ErrorAction Stop
-    	    Exit-AppveyorBuild
-    	}
-    	$currentVersion = $Matches[1]	
-    	$currentVersionSegments = $currentVersion.Split(".")	
-    	Write-Output "Current version: $currentVersion.* / $($currentVersionSegments.Count+1) parts"
-    }
+}
 
-    # Get new version from file
-    if($isPR -eq $true -or -not (Test-Path $ENV:VersionFile)) { return }
+# Extract version format
+function Extract-VersionsFormat {  
+    $currentVersion = $env:APPVEYOR_BUILD_VERSION
+    $env:VersionSegmentCount = $($currentVersion.Split(".")).Count
+    $env:buildVersion = $env:APPVEYOR_BUILD_VERSION -replace '\..*$', ''
+    $env:buildNumber = $env:APPVEYOR_BUILD_NUMBER
+   	Write-Output "Current version: $env:buildVersion.* / $($currentVersionSegments.Count) parts"
+}
+
+# Get new version from file
+function Get-VersionFromFile {
+    if($env:isPR -eq $true -or -not (Test-Path $env:VersionFile)) { return }
     
     Write-Output "Read new version from file"
     $versionPattern = "^(\s*\##?\s*v?)(?<version>\d+\.\d+\.\d+)"
     $fileContent = Get-Content -path "$env:VersionFile" -TotalCount 5
     foreach ($line in $fileContent) {
         if ($line -match $versionPattern) {
-            $newVersion = $matches['version']
+            $env:NewVersion = $matches['version']
             break
         }
     }    	
@@ -58,53 +47,71 @@ function Update-Version {
         Exit-AppveyorBuild
     }	
     $newVersionSegments = $newVersion.Split(".")	
-    Write-Output "New version: ""$newVersion.*"" / $($newVersionSegments.Count+1) parts"	
-    if($newVersionSegments.Count+1 -ne 4) {
+    if($newVersionSegments.Count+1 -ne $env:VersionSegmentCount) {
         $env:APPVEYOR_SKIP_FINALIZE_ON_EXIT="true"
         Write-Error -Message "`nERROR: Unsupported version format!" -ErrorAction Stop
         Exit-AppveyorBuild
-    }	
-    $buildVersion = $newVersion	    
-
-    # Check if new version is greater  
-    $reset_build = $false
-    if(($isPR -eq $true) -or -not ($newVersion)) { return }
-    
-    if ($newVersionSegments[0] -gt $currentVersionSegments[0]) {
-        $reset_build = $true
-        $buildNumber = 0
-    } elseif (($newVersionSegments[0] -eq $currentVersionSegments[0]) -and ($newVersionSegments[1] -gt $currentVersionSegments[1])) {
-        $reset_build = $true
-        $buildNumber = 0
-    }elseif (($newVersionSegments[0] -eq $currentVersionSegments[0]) -and ($newVersionSegments[1] -eq $currentVersionSegments[1]) -and ($newVersionSegments[2] -gt $currentVersionSegments[2])) {
-        $reset_build = $true
-        $buildNumber = 0
-    }
-    Write-Output "Reset build number: $reset_build"    
-
-    # Conditional update settings
-    if(($reset_build -eq $true) -and ($isPR -eq $false)) {
-        $b=$versionFormat -match "^(\d+\.\d+\.\d+)\.(.*)$"
-        $settings.versionFormat = "$buildVersion.$($Matches[2])"
-        Write-Output "Build version format: $($settings.versionFormat)"
-        $body = ConvertTo-Json -Depth 10 -InputObject $settings
-        $response = Invoke-RestMethod -Method Put -Uri "$apiUrl/projects" -Headers $appveyorApiRequestHeaders -Body $body
     }
 
-    # Conditional Send nextBuildNumber = 1
-    if(($reset_build -eq $true) -and ($isPR -eq $false)) {
-        $build = @{ nextBuildNumber = 1 }
-        $json = $build | ConvertTo-Json    
-        Write-Output "Invoke 'Reset Build Nummer'"
-        Invoke-RestMethod -Method Put "$apiUrl/projects/$env:APPVEYOR_ACCOUNT_NAME/$env:APPVEYOR_PROJECT_SLUG/settings/build-number" -Body $json -Headers $appveyorApiRequestHeaders
+    Write-Output "New version: ""$env:NewVersion.*"" / $($newVersionSegments.Count+1) parts"	
+    return $env:NewVersion
+}
+
+function Test-NewVersionIsCreater {
+    $currentVersionSegments = $env:buildVersion.Split(".")
+    $newVersionSegments = $env:newVersion.Split(".")
+
+    for ($i = 0; $i -lt $currentVersionSegments.Length; $i++) {
+        if ([int]$newVersionSegments[$i] -gt [int]$currentVersionSegments[$i]) { return $true } 
+        if ([int]$newVersionSegments[$i] -lt [int]$currentVersionSegments[$i]) { throw "New version is smaller than current version." }
+    }
+    return $false
+}
+
+function Reset-BuildNumber {
+    if(-not $env:AppVeyorApiUrl) {throw "env:AppVeyorApiUrl is empty."}
+    if(-not $env:AppVeyorApiRequestHeaders) {throw "env:AppVeyorApiRequestHeaders is empty."}
+
+    $env:buildNumber = 0
+    $json = @{ nextBuildNumber = 1 } | ConvertTo-Json    
+    Write-Output "Invoke 'Reset Build Nummer'"
+    Invoke-RestMethod -Method Put "$env:AppVeyorApiUrl/projects/$env:APPVEYOR_ACCOUNT_NAME/$env:APPVEYOR_PROJECT_SLUG/settings/build-number" -Body $json -Headers $env:AppveyorApiRequestHeaders
+} 
+
+function Update-AppVeyorSettings {
+    if(-not $env:AppVeyorSettings) {throw "env:AppVeyorSettings is empty."}
+    if(-not $env:AppVeyorApiUrl) {throw "env:AppVeyorApiUrl is empty."}
+    if(-not $env:AppVeyorApiRequestHeaders) {throw "env:AppVeyorApiRequestHeaders is empty."}
+
+    $env:AppVeyorSettings.versionFormat = "$env:buildVersion.{build})"
+    Write-Output "Build version format: $($env:AppVeyorSettings.versionFormat)"
+    $body = ConvertTo-Json -Depth 10 -InputObject $settings
+    $response = Invoke-RestMethod -Method Put -Uri "$env:AppVeyorApiUrl/projects" -Headers $env:AppVeyorApiRequestHeaders -Body $body
+}
+
+function Update-Version {
+	[CmdletBinding()]
+
+    if($env:isPR) { 
+        Extract-VersionsFormat
+        Write-Output ("INFO: Pull Request detected. skip Update-Version.")
+        return 
     }
 
-    # set current build version
-    if($isPR -eq $false) {
-        Update-AppveyorBuild -Version "$buildVersion.$buildNumber$versionSuffix$meta"
-    }
-	$env:buildVersion=$buildVersion
-	$env:buildNumber=$buildNumber
+	$isPR = $env:isPR
+	Write-Output "START: Update-Version()"
+	Write-Output "isPR: $isPR"
+	Write-Output "env:VersionFile: env:VersionFile"
+	
+    Init-AppVeyorApiRequest 	
+    Read-AppVeyorSettings	
+	Extract-VersionsFormat
+    var $newVersion = Get-VersionFromFile
+    if(-not $newVersion) { return }    
+    $env:buildVersion = $newVersion
+    if(Test-NewVersionIsGreater) { Reset-BuildNumber }
+    Update-AppVeyorSettings
+    Update-AppveyorBuild -Version "$env:buildVersion.$env:buildNumber$env:versionSuffix$env:versionMeta"
 	
     Write-Output "env:APPVEYOR_BUILD_VERSION: $env:APPVEYOR_BUILD_VERSION"
 	Write-Output "env:buildVersion: $env:buildVersion"
